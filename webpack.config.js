fe52e4b3d4de08829841b7a9989eee76a5c6f9d0
Chris Ahern <ahern.christopher@gmail.com>
2020-11-04 20:28:02 -0500

    Add initial files for Notes App

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: ['babel-polyfill', './Source/index.js'],
+        edit: ['babel-polyfill', './Source/edit.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'Public/Scripts'),
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/Scripts/'
+    },
+    devtool: 'source-map'
+}
